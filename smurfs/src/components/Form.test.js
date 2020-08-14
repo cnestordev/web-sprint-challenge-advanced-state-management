4a3884dc0c0dbbb105510b93325f5b53ccd74c91
Nestor <ncervan87@gmail.com>
2020-08-14 10:45:27 -0700

    testing form

--- smurfs/src/components/Form.test.js
+++ smurfs/src/components/Form.test.js
@@ -1,11 +1,7 @@
-import React from "react"
-import { render, screen, fireEvent, act } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import Form from './Form'
 
 // All inputs are visible
 test('check if all inputs are in the DOM', () => {
-    act(() => {
-        render(<Form />)
-    })
+    render(<Form />)
 })
-
